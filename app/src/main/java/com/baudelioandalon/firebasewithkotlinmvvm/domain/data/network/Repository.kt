5a3edbab4e533ff82276925210262d73cd211fce
Baudelio <baudelio_andalon@hotmail.com>
2020-06-07 19:40:24 -0500

    Working

--- app/src/main/java/com/baudelioandalon/firebasewithkotlinmvvm/domain/data/network/Repository.kt
+++ app/src/main/java/com/baudelioandalon/firebasewithkotlinmvvm/domain/data/network/Repository.kt
@@ -18,21 +18,21 @@ class Repository{
         val listData : MutableList<User> = mutableListOf()
         registration = docRef.addSnapshotListener { t, _ ->
             if (t!!.documents.isNotEmpty()){
-                    val data =
-                        User(
-                            t.documentChanges[0].document.getString("idKey")!!,
-                            t.documentChanges[0].document.getString("title")!!,
-                            t.documentChanges[0].document.getString("imageUri")!!,
-                            t.documentChanges[0].document.getString("description")!!
-                        )
-                    Log.e("metada",t!!.documentChanges[0].type.toString())
-                    if(t.documentChanges[0].type.toString() == "MODIFIED"){
-                        listData[listData.indexOfFirst { it.idKey == data.idKey }] = data
-                    }else if(t.documentChanges[0].type.toString() == "ADDED"){
-                        listData.add(data)
-                    }else if(t.documentChanges[0].type.toString() == "REMOVED"){
-                        listData.removeAt(listData.indexOfFirst { it.idKey == data.idKey })
-                    }
+                    val data = User(t.documentChanges[0].document.getString("idKey")!!, t.documentChanges[0].document.getString("title")!!,
+                            t.documentChanges[0].document.getString("imageUri")!!, t.documentChanges[0].document.getString("description")!!)
+                    Log.e("metadata", t.documentChanges[0].type.toString())
+                when (t.documentChanges[0].type.toString()){
+                    "MODIFIED" -> listData[listData.indexOfFirst { it.idKey == data.idKey }] = data
+                    "ADDED" -> listData.add(data)
+                    "REMOVED" -> listData.removeAt(listData.indexOfFirst { it.idKey == data.idKey })
+                }
+//                    if(t.documentChanges[0].type.toString() == "MODIFIED"){
+//                        listData[listData.indexOfFirst { it.idKey == data.idKey }] = data
+//                    }else if(t.documentChanges[0].type.toString() == "ADDED"){
+//                        listData.add(data)
+//                    }else if(t.documentChanges[0].type.toString() == "REMOVED"){
+//                        listData.removeAt(listData.indexOfFirst { it.idKey == data.idKey })
+//                    }
                 }else{
                 listData.clear()
             }
